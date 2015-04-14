818bdd1a50cfd28be567c1834e1c022a5a878c55
Wilson Cheng <wilzon@gmail.com>
2015-04-14 19:37:49 +0800

    first commit

--- js/application.js
+++ js/application.js
@@ -1,248 +1,142 @@
 $(document).ready(function() { 
 
 
-var postContent;
-
-  $('.searchButtonPost').click(function(){ // note: this will make the post even if some of the keys don't have any values.
-    // console.log($('.movieName').val()); would also have worked
-   // getURL();
-  postContentTitle = $('.postNameTitle').val();
-  postContentText = $('.postNameText').val();
-  postContentUser = $('.postNameUser').val();
-
-  var postRequest = { //note: the variable request1 has to be within the search button click function because you can't define a variable within request1. And you need to define movieName within the same scope.
-    type: "POST",
-    url: "http://ga-wdi-api.meteor.com/api/posts",
-    data: {
-      title: postContentTitle,
-      text:  postContentText,
-      user: postContentUser,
-    },
-    dataType: "JSON",
-    success: function(response) {
-      console.log("Great success", response);
-      }
-    }
-    $.ajax(postRequest);
-    }
-  );
-
-$('.postTitleButton').click(function(){
-  postContentTitle = $('.postNameTitle').val();
- 
-  var postRequest = { 
-    type: "POST",
-    url: "http://ga-wdi-api.meteor.com/api/posts",
-    data: {
-      title: postContentTitle,
-    },
-    dataType: "JSON",
-    success: function(response) {
-      console.log("Great post title success", response);
-      }
-    }     
-    $.ajax(postRequest);
-    }
-  );
-
-$('.getButton').click(function(){
-  getID = $('.getID').val();
- 
-  var getRequest = { //note: the variable request1 has to be within the search button click function because you can't define a variable within request1. And you need to define movieName within the same scope.
-    type: "GET",
-    url: "http://ga-wdi-api.meteor.com/api/posts/"+getID,
-    data: {
-    },
-    dataType: "JSON",
-    success: function(response) {
-      console.log("Great get title success", response);
-      // $('#getData').append(response);
-      $('#getDataTitle').append(response["title"]);
-      $('#getDataText').append(response["text"]);
-      $('#getDataUser').append(response["user"]);
-//      $('#getData').append(response); // this doesn't work because the response is an object. You need to go into each hash to get the relevant data.
-        }
-      }     
-      $.ajax(getRequest);
-    }
-  );
-
-  $('.putButton').click(function(){
-  putID = $('.putID').val();
- 
-  putTitle = $('.putInputTitle').val();
-  putText = $('.putInputText').val();
-  putUser = $('.putInputUser').val();
-
-  var putRequest = { 
-    type: "PUT",
-    url: "http://ga-wdi-api.meteor.com/api/posts/"+putID,
-    data: {
-      title: putTitle,
-      text:  putText,
-      user: putUser,  
-    },
-    dataType: "JSON",
-    success: function(response) {
-      console.log("Great put success", response);
-        }
-      }     
-      $.ajax(putRequest);
-    }
-  );
-
-$('.deleteButton').click(function(){
-  deleteID = $('.deleteID').val();
- 
-  var getRequest = { 
-    type: "DELETE",
-    url: "http://ga-wdi-api.meteor.com/api/posts/"+deleteID,
-    data: {
-    },
-    dataType: "JSON",
-    success: function(response) {
-      console.log("Great delete success", response);
-        }
-      }     
-      $.ajax(getRequest);
-    }
-  );
-
-
-  $('.getListingButton').click(function(){
-  var listRequest = { 
-    type: "GET",
-    url: "http://ga-wdi-api.meteor.com/api/posts/", //this URL is on GIT. If you should go Github, it's inside AJAX crud, 
-    //data: {},
-    dataType: "JSON",
-    success: function(response) {
-      console.log("Great get list success", response);
-      $('#all-posts > tbody').text(''); // wipe out the old list
-        // $('#all-posts > tbody').append(
-        //   "<tr class='post-item'>" + 
-        //   "<td>" + 
-        //   post._id + 
-        //   "</td>" + 
-        //   "<td>" + 
-        //   post.title + 
-        //   "</td>" + 
-        //   "<td>" + 
-        //   post.text + 
-        //   "</td>" + 
-        //   "<td>" + 
-        //   post.user + 
-        //   "</td>" + 
-        //   "</tr>");
-      response.forEach(function (post) {
-         $('#all-posts > tbody').append("<tr class='post-item'>" + "<td>" + post._id + "</td>" + "<td>" + post.title + "</td>" + "<td>" + post.text + "</td>" + "<td>" + post.user + "</td>" + "</tr>");
-      });
-    }
-  }
-    $.ajax(listRequest);
-  }
-  );
-
-});
+  $(document).on('click','.searchButton', function(){
+    getRequest();
+  });
   
+  $(document).on('click','.newButton', function(){
+    postRequest();
+  });
+  
+  $(document).on('click','.listButton', function(){
+    listRequest();
+  });
 
-        // $('#listTitle').append("<li>"+response[i]["title"]+"</li>");
-        // //$('#listTitle').append("<br>");
-        // $('#listText').append("<li>"+response[i]["text"]+"</li>");
-        // // $('#listText').append("<br>");
-        // $('#listUser').append("<li>"+response[i]["user"]+"</li>");
-        // // $('#listUser').append("<br>");
-            
-// }); 
+  $(document).on('click','.deleteButton', function(){
+    deleteRequest();
+  });
+
+  //search request
+  function getRequest() {
+    $.ajax({
+      type: "GET",
+      url: 'http://harry-quotes-216770.apse1.nitrousbox.com:8080/quotes/search/' +  $('.search-value').val(),    
+      dataType: "JSON",
+      success: function(response) {
+        console.log("Great success", response);
+        $('#all-posts > tbody').text(''); 
+        response.forEach(function (post) {
+          $('#all-posts > tbody').append("<tr class='post-item'>" + "<td>" + post._id + "</td>" + "<td>" + post.quote + "</td>" + "<td>" + post.author + "</td></tr>" );
+        });
+      }
+    }) 
+  }
+
+//post request
+  function postRequest() {
+    $.ajax({
+      type: "POST",
+      url: 'http://harry-quotes-216770.apse1.nitrousbox.com:8080/quotes',
+      data: {
+        quote: {
+          quote:  $('.newQuote').val(),
+          author: $('.newAuthor').val()
+        }      
+      },
+      dataType: "JSON",
+      success: function(response) {
+        console.log("Great success", response);
+        // $('#all-posts > tbody').text(''); 
+        // response(function (post) {
+        //   $('#all-posts > tbody').append("<tr class='post-item'>" + "<td>" + post._id + "</td>" + "<td>" + post.quote + "</td>" + "<td>" + post.author + "</td></tr>" );
+          listRequest();
+        }
+      
+    }) 
+  }
+
+  //list request
+  function listRequest() {
+    $.ajax({
+      type: "GET",
+      url: 'http://harry-quotes-216770.apse1.nitrousbox.com:8080/quotes',
+      dataType: "JSON",
+      success: function(response) {
+        console.log("Great listing success", response);
+        $('#all-posts > tbody').text(''); 
+        response.forEach(function (post) {
+          $('#all-posts > tbody').append("<tr class='post-item'>" + "<td>" + post._id + "</td>" + "<td>" + post.quote + "</td>" + "<td>" + post.author + "</td></tr>" );
+        });
+      }
+    }) 
+  }
+
+//delete request
+  // function deleteRequest() {
+  //   $.ajax({
+  //     type: "DELETE",
+  //     url: 'http://harry-quotes-216770.apse1.nitrousbox.com:8080/quotes/' +  $('.delete-value').val(),    
+  //     dataType: "JSON",
+  //     success: function(response) {
+  //       listRequest(); 
+  //       console.log("Great delete success!", response);
+  //     }
+  //   })
+  // }
+  
+  function deleteRequest() {
+    $.ajax({
+      type: "DELETE",
+      url: 'http://harry-quotes-216770.apse1.nitrousbox.com:8080/quotes/' +  $('.delete-value').val(),    
+      dataType: "JSON",
+      success: function(response) {
+          listRequest(); 
+          console.log("Great delete success!", response);
+      }
+      // error: function(response) {
+      //     console.log("you suck ass")
+      // }
+    })
+  }
+
+
+// success: function(response) {
+//     if (response == 'success') 
+//         window.location.replace("home");
+//     else if (response == "failed") 
+//         window.location.replace("failed");
+//     else 
+//         $("#message").html("<div class='error_log'><p class='error'>Invalid username and/or password.</p></div>");
+// }​
 
 
 
-// //**
-// html:
-// <table id="all-posts" class="table table-condensed">
-// <thead>
-// <tr>
-// <th>TD</th>
-// <th>Title</th>
-// <th>Text</th>
-// <th>User</th>
-// </tr>
-// <thead>
-// <tbody>
-// </tbody>
-// //**
+//pills
+ $('.add-div, .delete-div, .list-div').hide();
 
-// //for each is a for loop that goes thru an array
-// //for loop adds the new list
-// response.forEach(function (post) {
+ $(document).on('click','.searchPillBut', function() {
+   $('.delete-div, .list-div, .add-div').hide();
+   $('.search-div').show();
+ })
 
-//   $('#all-posts > tbody').append(
-//     "<tr class='post-item'>"+
-//       "<td>" + 
-//         post._id + 
-//       "</td>"+
-//       "<td>"+ 
-//         post.title + 
-//       "</td>"+
-//       "<td>" + 
-//         post.text + 
-//       "</td>"+
-//       "<td>" + 
-//         post.user + 
-//       "</td>"+
-//     "</tr>");
-// });
-// }
-// }
+ $(document).on('click','.addPillBut', function() {
+   // $(this).attr("class", "active");
+   $('.search-div, .delete-div, .list-div').hide();
+   $('.add-div').show();
+ })
 
-// function listAllPost() {
-//   $.ajax(listALlRequests)
-// }
+ $(document).on('click','.listPillBut', function() {
+   $('.search-div, .add-div, .delete-div').hide();
+   $('.list-div').show();
+ })
+
+ $(document).on('click','.deletePillBut', function() {
+   $('.search-div, .list-div, .add-div').hide();
+   $('.delete-div').show();
+ })
 
 
 
-// // var postLog = {
-// //   post1: {
-// //     title: "",
-// //     text: "", 
-// //     user: "",
-// //   },
-// //   post2: {
-// //     title: "",
-// //     text: "",
-// //     user: "",
-// //   },
-// //   post3: {
-// //     title: "",
-// //     text: "", 
-// //     user: "",
-// //    }, // note: more buttons/drinks can go here, if any
-// // };
 
-
-
-// var title; // define title outside so it can be read later...
-
-//   $('.searchButton1').click(function(){
-//     // console.log($('.movieName').val()); would also have worked
-//    // getURL();
-//   title = $('.movieName').val();
-
-//   var request1 = { //note: the variable request1 has to be within the search button click function because you can't define a variable within request1. And you need to define movieName within the same scope.
-//     type: "GET",
-//     url: "http://www.omdbapi.com/?t="+title,
-//     dataType: "JSON",
-//     success: function(response) {
-//       console.log("This is the response: ", response);
-//       $('#title').append(response["Title"]);
-//       $('#year').append(response["Year"]);
-//       $('#country').append(response["Country"]);
-//       $('#released').append(response["Released"]);
-//       $('#poster').attr("src", response["Poster"]); // this is change the attribute of the ID "src", to the image link of the poster.
-//     }
-//   }
-
-//     $.ajax(request1);
-//   }
-//   );
-
-// });
+ })
